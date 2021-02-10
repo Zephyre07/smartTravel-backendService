a7e8d06d5ae786956f44b196aa42939ba30510bd
CORP\muasif <muhammad.asif@capgemini.com>
2021-02-10 12:35:14 +0530

    initial commit

---
+++ src/main/java/com/smart/travel/smarttravelbackend/SmartTravelBackendApplication.java
@@ -0,0 +1,14 @@
+package com.smart.travel.smarttravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelBackendApplication.class, args);
+		System.out.println("Hello WOrld");
+	}
+
+}
---
+++ src/test/java/com/smart/travel/smarttravelbackend/SmartTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.travel.smarttravelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
